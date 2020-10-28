514393781f133252ee8bc252e58afed193f34bca
shay markovich <shaymark@gmail.com>
2020-10-28 09:28:10 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# get stock price from market api
+
+# every minuite get the price
+
+# decide if buy or sell or hold with this params:
+static:
+- stock symbol
+- history length
+- tolarrence buy
+- tollarence sell
+- cost buy
+- cost sell
+dynamic:
+- new stock price
+- stock history x minutes ago
+
+# make a calculation to see if previous desisions where right or worng
+
+# make a way to realy do the transaction with api
+
